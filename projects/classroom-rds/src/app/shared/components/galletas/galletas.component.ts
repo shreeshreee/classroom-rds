ee7a401d3f20147623d91f038f00f644963e8e77
Julio C. Melchor P <jcmelchorp@gmail.com>
2021-02-09 11:37:37 -0600

    cookies recipe  alert

--- projects/classroom-rds/src/app/shared/components/galletas/galletas.component.ts
+++ projects/classroom-rds/src/app/shared/components/galletas/galletas.component.ts
@@ -6,7 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./galletas.component.scss']
 })
 export class GalletasComponent implements OnInit {
-
+  dismissible = true;
+  alert: any = {
+    type: 'warning',
+    msg: `<div>
+    <h2>
+    <strong>Receta</strong>
+    </h2>
+    Consulta aquí los ingredientes y el resumen de la preparación de la receta de los Polvorones.
+    </div>`
+  };
   constructor() { }
 
   ngOnInit(): void {
